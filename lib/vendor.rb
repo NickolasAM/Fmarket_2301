18524be5fcf84b0d369fb730317ee2a8052c4b93
Nickolas Marquez <nickomarquez7@gmail.com>
2023-03-07 10:07:15 -0700

    Initialized class

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,3 +1,12 @@
-# class Vendor
+class Vendor
+  attr_reader :name,
+              :inventory
+  def initialize(name)
+    @name = name
+    @inventory = {}
+    # @check_stock = 0
+    # require'pry';binding.pry
+
+  end
   
-# end
+end
--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -2,22 +2,29 @@ require 'rspec'
 require './lib/item'
 require './lib/vendor'
 
-# RSpec.describe Vendor do
-#   describe '#Iteration 1' do
-#     it 'exists and haas attributes'do
-#     item1 = Item.new({name: 'Peach', price: "$0.75"})
-#     item2 = Item.new({name: 'Tomato', price: '$0.50'})
+RSpec.describe Vendor do
+  describe '#Iteration 1' do
+    it 'exists and haas attributes'do
+      vendor = Vendor.new("Rocky Mountain Fresh")
 
-#     expect(item1)to be_an_instance_of(Vendor)
-#     end
+      expect(vendor).to be_an_instance_of(Vendor)
+      expect(vendor.name).to eq("Rocky Mountain Fresh")
+      expect(vendor.inventory).to eq({})
+    end
+
+    # it 'vendor can check stock' do
+    #   vendor = Vendor.new("Rocky Mountain Fresh")
+    #   item1 = Item.new({name: 'Peach', price: "$0.75"})
+    #   item2 = Item.new({name: 'Tomato', price: '$0.50'})
+
+    #   expect(vendor.check_stock(item1)).to eq(0)
 
 
+    # end
 
+  end
 
-
-#   end
-
-# end
+end
 
 
 
